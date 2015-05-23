ff8cae3a280cdc069f258f537fdc1116cbfee34e
Matheus Ribeiro <matheusribeiro@Matheus-De-Melo.local>
2015-05-23 06:14:33 -0400

    Implementada a viewDidLoad e o btEditar da tela mostrarMomentoViewController

--- ThatPlace/MostrarEditarMomentoViewController.m
+++ ThatPlace/MostrarEditarMomentoViewController.m
@@ -15,25 +15,51 @@
 @property (weak, nonatomic) IBOutlet UITextField *tfTitulo;
 @property (weak, nonatomic) IBOutlet UITextView *tvDescricao;
 @property (weak, nonatomic) IBOutlet UITextField *tfData;
+@property (weak, nonatomic) IBOutlet UIButton *btSalvar;
+@property (weak, nonatomic) IBOutlet UIButton *btTirarFoto;
+@property (weak, nonatomic) IBOutlet UIButton *btAlbum;
 @end
 
 @implementation MostrarEditarMomentoViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-        self.imFoto.image = self.momento.foto;
-        self.tfTitulo.text = self.momento.titulo;
-        self.tvDescricao.text = self.momento.descricao;
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
-        [dateFormatter setTimeStyle:NSDateFormatterMediumStyle];
-        self.tfData.text = [dateFormatter stringFromDate:self.momento.data];
     
+    self.btSalvar.hidden = TRUE;
+    self.btAlbum.hidden = TRUE;
+    self.btTirarFoto.hidden = TRUE;
+    
+    self.tfTitulo.enabled = FALSE;
+    
+    if(self.momento.foto != NULL){ //O objeto tem foto?
+        self.imFoto.image = self.momento.foto;
+    }
+    self.tfTitulo.text = self.momento.titulo;
+    if (self.momento.descricao != NULL) {
+        self.tvDescricao.text = self.momento.descricao;
+    }
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+    [dateFormatter setTimeStyle:NSDateFormatterMediumStyle];
+    self.tfData.text = [dateFormatter stringFromDate:self.momento.data];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     
+}
+- (IBAction)btEditar:(id)sender {
+    self.btSalvar.hidden = FALSE;
+    self.btAlbum.hidden = FALSE;
+    self.btTirarFoto.hidden = FALSE;
+    
+    self.tfTitulo.enabled = TRUE;
+    self.tvDescricao.editable = TRUE;
+    self.tfData.enabled = TRUE;
+}
+- (IBAction)btAlbum:(id)sender {
+}
+- (IBAction)btTirarFoto:(id)sender {
 }
 - (IBAction)btVoltar:(id)sender {
     [[self navigationController]popViewControllerAnimated:TRUE];
@@ -49,12 +75,12 @@
           [alert show];
           [self.tfData becomeFirstResponder];
           return;
-          }else{
-              self.momento.foto = self.imFoto.image;
-              self.momento.titulo = self.tfTitulo.text;
-              self.momento.descricao = self.tvDescricao.text;
-              //self.momento.data = self.lbData.text;    NAO SEI AINDA COMO FAZER ISSO //ActionSheet
-          }
+    }else{
+        self.momento.foto = self.imFoto.image;
+        self.momento.titulo = self.tfTitulo.text;
+        self.momento.descricao = self.tvDescricao.text;
+        //self.momento.data = self.lbData.text;    NAO SEI AINDA COMO FAZER ISSO //ActionSheet
+    }
     [[self navigationController]popViewControllerAnimated:TRUE];
 }
 
